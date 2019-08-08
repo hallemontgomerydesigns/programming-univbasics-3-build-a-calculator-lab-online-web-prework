8bc0532781090e675ae747261b5925dff7377020
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-08-08 17:16:26 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,16 +1,12 @@
 def addtion(num1, num2)
-  num1 = 20 
-  num2 = 16
   return num1 + num2
 end
-
+addition(8,9)
 def subtraction(num1, num2)
-  num1 = 20 
-  num2 = 16
+ 
   return num1 - num2
-  
 end
-
+ subtraction(9,3)
 def multiplication(num1,num2)
 
   return num1 * num2
@@ -19,11 +15,16 @@ end
 def divisiion (num1, num2)
   return num1 - num2
 end
-
+ division (8,2)
 def modulo (num1, num2)
   return num1 % num2
 end
+ modulo(50,16)
 
 def square_root(num)
    
+   return num**2
+ end
+ 
+ 
   
