0ff78ae48493dbc1c08d9e9fa2f404e47b36af3d
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-08-08 17:29:39 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -17,17 +17,16 @@ end
 def divisiion (num1, num2)
   return num1 - num2
 end
- division (8,2)
  
 def modulo (num1, num2)
   return num1 % num2
 end
- modulo(50,16)
+
 
 def square_root(num)
    
    return num**2
  end
- square_root(81) 
+ 
  
   
